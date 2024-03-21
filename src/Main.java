2a97ca469443bf99299ee3c89d0588994a4336b9
Alexander P <aprechtel0043@gmail.com>
2024-03-21 00:41:33 -0400

    i dont even know

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
         System.out.println("Please enter the desired difficulty");
         System.out.println("\t1. Easy (1-3)");
         System.out.println("\t2. Medium (4-6)");
         System.out.println("\t3. Hard (6+)");
 
-        Words wordGame = new Words();
+        words wordGame = new words();
         Scanner scnr = new Scanner(System.in);
         int choice = scnr.nextInt();
 
         switch(choice){
             case(1):
                 System.out.println("You have selected Easy mode, Begin!");
-                wordGame = new Words("easy");
+                wordGame = new words("easy");
                 break;
             case(2):
                 System.out.println("You have selected Medium mode, Begin!");
-                wordGame = new Words("medium");
+                wordGame = new words("medium");
                 break;
             case(3):
                 System.out.println("You have selected Hard mode, Begin!");
-                wordGame = new Words("hard");
+                wordGame = new words("hard");
                 break;
             default:
                 System.out.println("Error, input is not recognized.");
         } // end switch
+        System.out.println(wordGame.getWord());
         wordGame.start();
     } // end main method
 } // end class
