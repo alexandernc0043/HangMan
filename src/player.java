9e97c65af1170353ff560aeddd31f3241d63db7c
Alexander <aprechtel0043@gmail.com>
2024-03-22 15:51:41 -0400

    finished player class

--- src/player.java
+++ src/player.java
@@ -1,6 +1,7 @@
 public class player {
     private String name;
     private int gamesWon;
+    private int gamesPlayed;
 
     public String getName() {
         return name;
@@ -16,13 +17,22 @@ public class player {
     public void setGamesWon(int gamesWon) {
         this.gamesWon = gamesWon;
     }
-    public player(String name, int gamesWon){
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public player(String name, int gamesWon, int gamesPlayed){
         this.name = name;
         this.gamesWon = gamesWon;
-
+        this.gamesPlayed = gamesPlayed;
     }
 
     public void displayPlayerStats(player plr, words game){
-        System.out.println("Player: " + plr.getName() +"\nGames Won");
+        System.out.println("Player: " + plr.getName() +"\nGames Won: " + getGamesWon() + "\nGames Played: " + getGamesPlayed());
     }
 }
