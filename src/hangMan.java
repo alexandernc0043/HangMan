acd33b1ba84367cab6c73df88a59788c342236e8
Alexander P <144269917+alexandernc0043@users.noreply.github.com>
2024-04-04 05:10:45 -0400

    Update hangMan.java
    removed unnecessary colors

--- src/hangMan.java
+++ src/hangMan.java
@@ -3,9 +3,6 @@ public class hangMan {
     // Credits to https://github.com/YungNewton for the HangMan Art
     String RESET = "\u001B[0m";
     String RED = "\u001B[31m";
-    String GREEN = "\u001B[32m";
-    String YELLOW = "\u001B[33m";
-    String BLUE = "\\u001B[31m";
     String[] HANGMANPICS = {"""
    +--+
    |  |
